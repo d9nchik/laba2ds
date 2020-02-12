017ff7c7f71c9a076ea2fe70a4a39de198b016bc
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-12 23:01:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,103 @@
+import copy
+
+
+def get_data():
+    data = []
+    with open("input.txt") as iFile:
+        while True:
+            line = iFile.readline()
+            if not line:
+                break
+            temp = (line[:len(line)] + line[len(line) + 1:]).split()
+            for x in range(len(temp)):
+                temp[x] = int(temp[x])
+            data.append(temp)
+            print(line, end='')
+        print()
+    return data
+
+
+def create_adjacency_matrix(idata):
+    matrix = [0] * idata[0][0]
+    for x in range(idata[0][0]):
+        matrix[x] = [0] * idata[0][0]
+
+    for y in range(1, len(idata)):
+        matrix[idata[y][0] - 1][idata[y][1] - 1] = 1
+        matrix[idata[y][1] - 1][idata[y][0] - 1] = 1
+
+    return matrix
+
+
+def show_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("%2d" % matrix[i][j], end=" ")
+        print()
+    print()
+
+
+def pow_matrix(matrix1, matrix2):
+    temp = copy.deepcopy(matrix2)
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1)):
+            sum_of_matrix = 0
+            for z in range(len(matrix1)):
+                sum_of_matrix += matrix1[i][z] * matrix2[z][j]
+            temp[i][j] = sum_of_matrix
+    return temp
+
+
+def get_move_matrix(matrix):
+    gold_image = copy.deepcopy(matrix)
+    in_degree = copy.deepcopy(matrix)
+    for x in range(1, len(matrix)):
+        in_degree = pow_matrix(in_degree, gold_image)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] > 0 or i == j:
+                    continue
+                if in_degree[i][j] > 0:
+                    matrix[i][j] = x + 1
+    show_matrix(matrix)
+    for j in range(len(matrix)):
+        for m in range(len(matrix[0])):
+            if matrix[j][m] == 0 and m != j:
+                matrix[j][m] = -1
+
+    return matrix
+
+
+def find_diameter(move_matrixy):
+    digits = []
+    for i in range(len(move_matrixy)):
+        maximum = move_matrixy[i][0]
+        for j in range(len(move_matrixy)):
+            if maximum < move_matrixy[i][j]:
+                maximum = move_matrixy[i][j]
+        digits.append(maximum)
+    maximum = digits[0]
+    minimum = digits[0]
+    for x in range(1, len(digits)):
+        if maximum < digits[x]:
+            maximum = digits[x]
+        elif (minimum > digits[x] > 0) or (minimum < 1 and digits[x] > 0):
+            minimum = digits[x]
+    print("Діаметр графу: %d" % maximum)
+    print("Радіус графа: %d" % minimum)
+    print("Вершини графу:", end=' ')
+    for x in range(len(digits)):
+        if minimum == digits[x]:
+            print(x + 1, end=" ")
+
+
+graph = get_data()
+adjacency = create_adjacency_matrix(graph)
+print("Матриця суміжності: ")
+show_matrix(adjacency)
+print("Матриця відстані: ")
+move_matrix = get_move_matrix(adjacency)
+print("Матриця досяжності: ")
+show_matrix(move_matrix)
+
+find_diameter(move_matrix)
